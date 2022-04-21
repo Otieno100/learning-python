8ef8dbd2eebee8a8c049c179097a21a02fa70139
Otieno100 <brian.masafu@student.moringaschool.com>
2022-04-22 01:51:18 +0300

    created first python-project

---
+++ test.py
@@ -0,0 +1,24 @@
+
+class Contact:
+    """
+    Class that generates new instances of contacts.
+    """
+
+    contact_list = [] # Empty contact list
+
+    def __init__(self,first_name,last_name,number,email):
+
+      # docstring removed for simplicity
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = number
+        self.email = email
+
+        
+    new_contact =__init__("brian","otieno","0790906666", "b@108")  
+
+ 
+    print(new_contact.first_name )
+
+      
